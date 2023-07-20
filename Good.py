c44e520fa4d1aa6e8ae3a6008ef553e1e2f315d4
Ali-Borhani <117738373+A-brhn@users.noreply.github.com>
2023-07-20 19:24:11 +0430

    created Good class

---
+++ Good.py
@@ -0,0 +1,8 @@
+class Good:
+
+    def __init__(self, name, price, quantity, productiondate, expirationdate):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.productionDate = productiondate
+        self.expirationDate = expirationdate
